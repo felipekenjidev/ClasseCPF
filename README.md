3c64840b08cbaaf1b911a4f70091c8e69aedc5f2
Felipe Kenji Ouba Fukuzono <160082832+felipekenjidev@users.noreply.github.com>
2024-02-14 21:48:14 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ClasseCPF
+# Classe C# para Validação de CPF
 Uma Classe C# com métodos e propriedades para realizar a verificação do CPF (Cadastro de Pessoa Física), formatando seus dígitos e indicando se o documento digitado é Verdadeiro ou Falso.
+
+# Preparação
+Antes de poder utilizar a Classe CPF, é necessário importar para dentro do projeto, colocando o próprio arquivo `clsCPF.cs` dentro do projeto ou adicionando uma referência para a DLL `clsCPF.dll`.
+
+# Como Funciona?
+Para utilizar basta instanciar a Classe CPF, passando no parâmetro o CPF a ser verificado. Pode passar o CPF tanto com as pontuações como `696.197.530-54` ou sem como `69619753054`.
+
+`CPF variavel = new CPF("69619753054");`
+
+Após instanciar a Classe, apenas é preciso acessar o método `Validar()` que o resultado da verificação será gerado.
+
+`bool resposta = variavel.Validar()`
---
+++ clsCPF.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Classe com Métodos de Validação de CPF
+/// </summary>
+public class CPF
+{
+    /// <summary>
+    /// Construtor da Classe para formatação de Dígitos
+    /// </summary>
+    /// <param name="digitos">Dígitos do CPF</param>
+    /// <exception cref="ArgumentException">Erro adquirido ao inserir dígitos que não correspondam a um CPF</exception>
+    public CPF(string digitos)
+    {
+        //Verificação do Formato do CPF Inserido
+        digitos = digitos.Replace(".", "").Replace("-", "").Trim();
+        if (String.IsNullOrEmpty(digitos))
+            throw new ArgumentException("O CPF não pode ser vazio.");
+        if (digitos.Length != 11)
+            throw new ArgumentException("O CPF deve possuir todos os 11 dígitos.");
+        if (!long.TryParse(digitos, out long digitosFormatados))
+            throw new ArgumentException("O CPF deve ser composto somente por dígitos numéricos.");
+
+        //Guardar os Dígitos do CPF
+        this.Digitos = digitos;
+    }
+
+    /// <summary>
+    /// Propriedade que guarda todos os Dígitos do CPF
+    /// </summary>
+    public string Digitos { get; set; }
+
+    /// <summary>
+    /// Método que retorna a validade do Documento CPF
+    /// </summary>
+    /// <returns>Retorna a Validade do Documento</returns>
+    public bool Validar()
+    {
+        //Caso haja somente números iguais, retornar que é Falso o Documento
+        if (this.Digitos == new string(Convert.ToChar(this.Digitos.Substring(0, 1)), 11))
+            return false;
+
+        //Gerar o CPF Válido e Padrão
+        int digitoVerificador1 = GerarDigitoVerificador(this.Digitos.Substring(0, 9));
+        int digitoVerificador2 = GerarDigitoVerificador(this.Digitos.Substring(0, 9) + digitoVerificador1);
+        string CPFReal = Digitos.Substring(0, 9) + digitoVerificador1.ToString() + digitoVerificador2.ToString();
+
+        //Enviar resposta da Validação
+        return this.Digitos == CPFReal ? true : false;
+    }
+
+    //Método que realiza os cálculos para gerar os Dígitos Verificadores
+    private int GerarDigitoVerificador(string digitos)
+    {
+        //Calcular as Multiplicações de cada Dígito do CPF
+        int soma = 0;
+        int multiplicador = digitos.Length + 1;
+        for (int indice = 0; indice < digitos.Length; indice++)
+        {
+            soma += int.Parse(digitos.Substring(indice, 1)) * multiplicador;
+            multiplicador--;
+        }
+
+        //Retornar o Dígito Validador
+        int resto = soma % 11;
+        if (resto > 1)
+            return 11 - resto;
+        else
+            return 0;
+    }
+}
